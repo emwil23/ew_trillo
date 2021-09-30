18a73abb05bdfba4c22b656ba0440300059a30c7
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2021-09-30 16:16:13 +0530

    Updates

---
+++ static/js/main.6d487da8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpew_trillo=this.webpackJsonpew_trillo||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);n(1);var r=n(3),c=n.n(r),i=(n(8),n(0)),s=function(){return Object(i.jsx)("div",{children:Object(i.jsx)("h1",{children:"Hey! This is a TEST!"})})};c.a.render(Object(i.jsx)(s,{}),document.querySelector("#root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.6d487da8.chunk.js.map
